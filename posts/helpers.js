7c6c8e08e85e668a1912428249c48f31ba5af6ee
Devin Dias <devindias.tech@gmail.com>
2020-03-20 17:16:47 -0500

    setting up boiler plate

--- posts/helpers.js
+++
@@ -1,5 +0,0 @@
-const db = require("../data/KnexConfig")
-
-module.exports = {
-  
-  };
--- secrets.js
+++ secrets.js
@@ -1,6 +1,6 @@
 module.exports = {
     jwtSecret: process.env.JWT_SECRET || `Agoodsoldierneverstealsseccrets`,
-    PORT: process.env.PORT || 5000,
+    PORT: process.env.PORT || 5300,
     env: process.env.DB_ENV || "development"
   };
   
